4ad26d98582f08ade4b52d620c34dbdff45bda6a
hadesong <hadesong@gmail.com>
2015-08-06 00:33:26 +0800

    专治各种不服

--- test.py
+++ test.py
@@ -15,3 +15,8 @@ print r'\\\\\\t\\' #Python 允许使用r''表示''内部的字符串默认不转
 for x in xrange(1,10):
 	print x
 
+
+def func(a , b , c=10 , *args , **kw):
+	print a , b , c , args , kw
+
+func(3 , 4 , 55 , 3 , 'a' , 'r' , 41 , x='gg' , y='bb' , z='jj')
