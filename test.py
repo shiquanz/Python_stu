273a16903012a4fe1e543e624896bd90564965cf
hadesong <hadesong@gmail.com>
2015-08-05 22:03:27 +0800

    Updata

---
+++ test.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+##########################################
+##                                      ##
+##            专治各种不服              ##
+##                                      ##
+##########################################
+
+
+print 'I'+' am OK!!'
+print "I\''' am allright!!"
+print "I\'m learning Python \n \tnow and ever...."
+print r'\\\\\\t\\' #Python 允许使用r''表示''内部的字符串默认不转义
+
+
+for x in xrange(1,10):
+	print x
+
