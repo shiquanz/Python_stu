027d7960cf68a24088bc0b096bf202ce5727e4ad
sugar黑人 <shiquan.zhao@foxmail.com>
2019-12-29 15:03:18 +0800

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,16 +1,14 @@
-# _*_ coding: utf-8 _*_
-
 # 我是单行注释
+# _*_ coding: utf-8 _*_/# coding=utf-8
 
 # Quick Python Script Explanation for Progeammers
 # 给程序员的超快Python脚本解说
 
-import os	 		#导入代码模块 os.py
+import os #导入代码模块 os.py
 
 def main():			
 	#函数名main在这里并不是必须的，调用在这段脚本的最后部分
 	#Python使用缩进来代替其他语句块声明，一般建议每个层级用4个空格来缩进
-
 	print ("Hello World")
 	#声明单行字符串，使用单、双引号都可以，注意对字符串中的引号进行转义处理
 	print ("这是Alice\'的问候")		
@@ -23,7 +21,7 @@ def main():
 	#调用了os模块中的函数，用 + 连接字符串
 	print ('这将直接执行'+os.getcwd()) 
 
-
+	
 	#变量需要先实例化才可进一步计算
 	counter = 0
 	counter += 1
@@ -50,7 +48,8 @@ def foo(paraml , secondParam):
 	res = paraml+secondParam
 	#字符串的格式化输出基本类似于C语言
 	print ('%s 加 %s 等于 %s ' %(paraml , secondParam , res))
-	
+	#或字符串的format方法输出
+	print('{} 加 {} 等于 {}'.format(paraml, secondParam, res))
 	#判定变量大小 于C语言相同
 	#用冒号结束判断句，在if elif else 行最后
 	if res < 50:
@@ -81,8 +80,6 @@ def ioFun():
 if __name__=='__main__':
 	main()
 	ioFun()
-
-
 
 
 
